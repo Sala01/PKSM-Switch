c05f8aeac0540f28dd6ed2cd352d92d8a12f54e9
Nico <168318133+NicoPlaysThis@users.noreply.github.com>
2026-02-28 09:32:01 -0700

    Add OUTPUT_TO_FILE var in Logger to write logs to sdmc:/switch/PKSM/pksm.log

--- include/utils/Logger.hpp
+++ include/utils/Logger.hpp
@@ -3,48 +3,41 @@
 #include <string>
 #include <switch.h>
 
-namespace pksm::utils {
+namespace pksm::utils
+{
 
-class Logger {
-public:
-    enum class Level { Debug, Info, Warning, Error };
+    class Logger
+    {
+    public:
+        enum class Level
+        {
+            Debug,
+            Info,
+            Warning,
+            Error
+        };
 
-#ifdef NDEBUG
-    static inline void Initialize() {}
-    static inline void Finalize() {}
-    static inline void Debug(const std::string&) {}
-    static inline void Info(const std::string&) {}
-    static inline void Warning(const std::string&) {}
-    static inline void Error(const std::string&) {}
-#else
-    static void Initialize();
-    static void Finalize();
-    static void Debug(const std::string& message);
-    static void Info(const std::string& message);
-    static void Warning(const std::string& message);
-    static void Error(const std::string& message);
-    static void LogMemoryInfo();
+        static void Initialize();
+        static void Finalize();
+        static void Debug(const std::string &message);
+        static void Info(const std::string &message);
+        static void Warning(const std::string &message);
+        static void Error(const std::string &message);
+        static void LogMemoryInfo();
 
-private:
-    static void Log(Level level, const std::string& message);
-    static bool initialized;
-    static bool socket_initialized;
-    static bool console_initialized;
-#endif
-};
+        static int OUTPUT_TO_FILE;
 
-}  // namespace pksm::utils
+    private:
+        static void Log(Level level, const std::string &message);
+        static bool initialized;
+        static bool socket_initialized;
+        static bool console_initialized;
+    };
 
-#ifdef NDEBUG
-#define LOG_DEBUG(msg) ((void)0)
-#define LOG_INFO(msg) ((void)0)
-#define LOG_WARNING(msg) ((void)0)
-#define LOG_ERROR(msg) ((void)0)
-#define LOG_MEMORY() ((void)0)
-#else
+} // namespace pksm::utils
+
 #define LOG_DEBUG(msg) pksm::utils::Logger::Debug(msg)
 #define LOG_INFO(msg) pksm::utils::Logger::Info(msg)
 #define LOG_WARNING(msg) pksm::utils::Logger::Warning(msg)
 #define LOG_ERROR(msg) pksm::utils::Logger::Error(msg)
 #define LOG_MEMORY() pksm::utils::Logger::LogMemoryInfo()
-#endif
